1c925ff2680aaa3b0d727f27ebefeff98359e8ac
Sietze Keuning <info@kingwebsites.nl>
2021-04-13 13:52:12 +0200

    Update TableRow.vue

--- src/components/TableRow.vue
+++ src/components/TableRow.vue
@@ -1,12 +1,7 @@
 <template>
-    <tr 
-        @click="clicked" 
-        @mouseover="over"
-        @mouseout="out"
-        :class="rowClass(row.data)"
-    >
+    <tr @click="clicked" @mouseover="over" @mouseout="out" :class="rowClass(row.data)" class="group">
         <table-cell
-            v-for="(column) in columns"
+            v-for="column in columns"
             :show="!column.hidden"
             :key="row.vueTableComponentInternalRowId + '-' + column.index"
             :row="row"
@@ -16,39 +11,38 @@
 </template>
 
 <script>
-        // v-tooltip.bottom.start="{content: tooltip, delay: 0}"
-    import TableCell from './TableCell';
+// v-tooltip.bottom.start="{content: tooltip, delay: 0}"
+import TableCell from './TableCell'
 
-    export default {
-        props: ['columns', 'row', 'tooltipField', 'rowClass'],
+export default {
+    props: ['columns', 'row', 'tooltipField', 'rowClass'],
 
-        components: {
-            TableCell,
+    components: {
+        TableCell,
+    },
+
+    computed: {
+        visibleColumns() {
+            return this.columns.filter((column) => !column.hidden)
         },
 
-        computed: {
-            visibleColumns() {
-                return this.columns.filter(column => ! column.hidden);
-            },
+        tooltip() {
+            if (!this.tooltipField) return
 
-            tooltip(){
-                if(!this.tooltipField) return 
-
-                return this.row.data[this.tooltipField]
-            },
+            return this.row.data[this.tooltipField]
         },
+    },
 
-        methods: {
-            clicked(){
-                this.$emit('click', this.row)
-            },
-            over(){
-                this.$emit('over', this.row)
-            },
-            out(){
-                this.$emit('out', this.row)
-            },
-        }
-
-    };
+    methods: {
+        clicked() {
+            this.$emit('click', this.row)
+        },
+        over() {
+            this.$emit('over', this.row)
+        },
+        out() {
+            this.$emit('out', this.row)
+        },
+    },
+}
 </script>
